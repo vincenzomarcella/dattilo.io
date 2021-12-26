2951ad62b1781ec71116676118399f8c6162e0ad
Vincenzo Marcella <marcella.vincenzo2@gmail.com>
2021-12-26 23:33:37 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Typewriter">
+        <textarea className="Typewriterinput" wrap="soft" autoFocus="true" placeholder='Type here...'></textarea>
+      </div>
+      <footer>
+        <p><small>&#169; Vincenzo Marcella 2022</small></p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
